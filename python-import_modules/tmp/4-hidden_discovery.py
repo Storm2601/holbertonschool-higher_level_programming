f5d0a22c9589870c61cde6f54850e34c715aeca2
root <root@Kvin2601>
2024-05-10 19:09:15 +0200

    prints all the names defined by the compiled module hidden_4.pyc, You should print only names that do not start with __

--- python-import_modules/tmp/4-hidden_discovery.py
+++ python-import_modules/tmp/4-hidden_discovery.py
@@ -2,6 +2,7 @@
 if __name__ == "__main__":
     import hidden_4
 
-    for name in dir(hidden_4):
-        if name[0:2] != "__":
+    module_names = dir(hidden_4)
+    for name in module_names:
+        if name[:2] != "__":
             print(name)
