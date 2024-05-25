eaa7c90bbd4a8e2167752ef4631ddc26f87343af
root <root@Kvin2601>
2024-05-26 01:25:28 +0200

    Create an abstract class named Shape with two abstract methods: area and perimeter, Implement two concrete classes: Circle and Rectangle, both inheriting from Shape. Each class should provide implementations for the area and perimeter methods, Write a standalone function named shape_info that accepts an object of type Shape (by duck typing) and prints its area and perimeter., Test the shape_info function with instances of both Circle and Rectangle

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""class shape with abstract method area and perimeter"""
+
+
+import math
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    """abstract class from ABC"""
+    @abstractmethod
+    def area(self):
+        """abstract method area"""
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        """abstract method perimeter"""
+        pass
+
+
+class Circle(Shape):
+    """A class representing a circle, derived from the Shape class."""
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        """Calculate the area of the circle."""
+        return abs(math.pi * (self.radius ** 2))
+
+    def perimeter(self):
+        """Calculate the perimeter of the circle."""
+        return abs(2 * math.pi * self.radius)
+
+
+class Rectangle(Shape):
+    """class rectangle from shape with two methods are and perimeter"""
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        """return area using width and height from init"""
+        return (self.width * self.height)
+
+    def perimeter(self):
+        """return perimeter adding width and height by two"""
+        return (2 * (self.width + self.height))
+
+
+def shape_info(Shape):
+    """shape method for duck typing"""
+    print(f"Area: {Shape.area()}")
+    print(f"Perimeter: {Shape.perimeter()}")
