fe6a57358935c05f18c27efc1c70701ba1bba5b5
root <root@Kvin2601>
2024-05-26 01:21:33 +0200

    Create an abstract class named Animal using the ABC package. This class should have an abstract method called sound, Create two subclasses of Animal: Dog and Cat. Implement the sound method in each subclass to return the strings “Bark” and “Meow” respectively

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Abstract Class"""
+
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """Abstract Class"""
+    @abstractmethod
+    def sound(self):
+        pass
+
+
+class Dog(Animal):
+    """subclass named Dog"""
+    def sound(self):
+        return ("Bark")
+
+
+class Cat(Animal):
+	"""class representing a cat, which is a subclass of Animal"""
+	def sound(self):
+		return "Meow"
