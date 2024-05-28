a44c84e5ebfda6acd32179fdffc2e1f2a37fa6b8
root <root@Kvin2601>
2024-05-28 08:41:15 +0200

    Write a function that reads a text file (UTF8) and prints it to stdout: You must use the with statement, You don’t need to manage file permission or file doesn't exist exceptions

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that print a file.txt"""
+
+
+def read_file(filename=""):
+    """open and print a file .txt"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
