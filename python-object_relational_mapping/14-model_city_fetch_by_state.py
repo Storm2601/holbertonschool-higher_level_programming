e0697a82810b727ca8a31dc7c303e0f3180adb46
root <root@Kvin2601>
2024-06-23 18:20:46 +0200

    Write a Python file similar to model_state.py named model_city.py that contains the class definition of a City

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,27 +1,39 @@
-from os import name
-from tkinter import INSERT
+#!/usr/bin/python3
+"""
+This script prints all City objects from the database hbtn_0e_14_usa
 
+Args:
+    - username (str): The username for the MySQL database.
+    - password (str): The password for the MySQL database.
+    - database (str): The name of the MySQL database
+    containing the 'cities' table
 
--- Create database hbtn_0e_14_usa, tables states and cities + some data
-CREATE DATABASE IF NOT EXISTS hbtn_0e_14_usa;
-USE hbtn_0e_14_usa;
+The script connects to a MySQL server running on localhost at port 3306.
+Results are sorted in ascending order by cities.id.
+Results are displayed as <state name>: (<city id>) <city name>
+"""
 
-CREATE TABLE IF NOT EXISTS states ( 
-    id INT NOT NULL AUTO_INCREMENT, 
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
-);
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
 
-CREATE TABLE IF NOT EXISTS cities ( 
-    id INT NOT NULL AUTO_INCREMENT, 
-    state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY(state_id) REFERENCES states(id)
-);
-INSERT INTO cities (state_id, name) VALUES (1, "San Francisco"), (1, "San Jose"), (1, "Los Angeles"), (1, "Fremont"), (1, "Livermore");
-INSERT INTO cities (state_id, name) VALUES (2, "Page"), (2, "Phoenix");
-INSERT INTO cities (state_id, name) VALUES (3, "Dallas"), (3, "Houston"), (3, "Austin");
-INSERT INTO cities (state_id, name) VALUES (4, "New York");
-INSERT INTO cities (state_id, name) VALUES (5, "Las Vegas"), (5, "Reno"), (5, "Henderson"), (5, "Carson City");
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        username, password, database), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    results = session.query(City, State).join(
+        State, City.state_id == State.id).order_by(City.id).all()
+
+    for city, state in results:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    session.close()
