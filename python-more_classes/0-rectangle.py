523d17d1668f38ea86e443ef377c2cfff7db0ee8
Storm2601 <kevinguedon2601@gmail.com>
2024-09-21 16:05:42 +0200

    Write an empty class Rectangle that defines a rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module for defining a simple geometric class.
+"""
+
+
+class Rectangle:
+    """
+    An empty class used to represent a Rectangle.
+    This class is currently a placeholder
+    and does not have any properties or methods.
+    """
