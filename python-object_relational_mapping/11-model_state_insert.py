8dae288bd7826f77625cb3ab165ea05c3c16af2c
Storm2601 <kevinguedon2601@gmail.com>
2024-10-31 17:13:04 +0100

    Write a script that adds the State object “Louisiana” to the database hbtn_0e_6_usa
        Your script should take 3 arguments: mysql username, mysql password and database name
        You must use the module SQLAlchemy
        You must import State and Base from model_state - from model_state import Base, State
        Your script should connect to a MySQL server running on localhost at port 3306
        Print the new states.id after creation
        Your code should not be executed when imported

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -22,13 +22,13 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     # Retrieve command line arguments for MySQL connection
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_database = sys.argv[3]
 
     # Create a SQLAlchemy engine to connect to the MySQL database
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        username, password, database), pool_pre_ping=True)
+        mysql_username, mysql_password, mysql_database), pool_pre_ping=True)
 
     # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
