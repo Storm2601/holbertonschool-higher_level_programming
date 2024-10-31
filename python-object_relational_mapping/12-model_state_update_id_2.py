9d5fdb5072a27e8de422df3453bd3be6a505b874
Storm2601 <kevinguedon2601@gmail.com>
2024-10-31 17:15:57 +0100

    Write a script that changes the name of a State object from the database hbtn_0e_6_usa
        Your script should take 3 arguments: mysql username, mysql password and database name
        You must use the module SQLAlchemy
        You must import State and Base from model_state - from model_state import Base, State
        Your script should connect to a MySQL server running on localhost at port 3306
        Change the name of the State where id = 2 to New Mexico
        Your code should not be executed when imported

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+This script changes the name of the State object
+with id = 2 to "New Mexico" in the database hbtn_0e_6_usa.
+
+It takes three arguments:
+    mysql_username: Your MySQL username.
+    mysql_password: Your MySQL password.
+    database_name: The name of the database to connect to.
+
+The script connects to a MySQL server running on localhost at port 3306.
+"""
+
+# Import the sys module for command line arguments
+import sys
+# Import create_engine for DB connection
+from sqlalchemy import create_engine
+# Import sessionmaker for ORM sessions
+from sqlalchemy.orm import sessionmaker
+# Import Base and State from model_state
+from model_state import Base, State
+
+if __name__ == "__main__":
+    # Retrieve command line arguments for MySQL connection
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_database = sys.argv[3]
+
+    # Create a SQLAlchemy engine to connect to the MySQL database
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        mysql_username, mysql_password, mysql_database), pool_pre_ping=True)
+
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+    # Create a session instance
+    session = Session()
+
+    # Query to retrieve the state with id = 2
+    update = session.query(State).filter_by(id=2).first()
+
+    # Update the state name to "New Mexico" if found
+    if update:
+        update.name = "New Mexico"
+        session.commit()
+
+    # Close the session to free resources
+    session.close()
