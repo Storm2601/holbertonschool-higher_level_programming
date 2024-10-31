562efedc915721abdc7fdd8fb279cdceec68500c
Storm2601 <kevinguedon2601@gmail.com>
2024-10-31 15:11:22 +0100

    Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
        Your script should take 4 arguments: mysql username, mysql password, database name and state name searched (no argument validation needed)
        You must use the module MySQLdb (import MySQLdb)
        Your script should connect to a MySQL server running on localhost at port 3306
        You must use format to create the SQL query with the user input
        Results must be sorted in ascending order by states.id
        Results must be displayed as they are in the example below
        Your code should not be executed when imported

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+"""
+This script takes in an argument and displays all values in the states table
+of the database hbtn_0e_0_usa where the name matches the argument.
+
+Arguments:
+    mysql_username: MySQL username.
+    mysql_password: MySQL password.
+    database_name: Name of the database to connect to.
+    state_name: Name of the state to search for.
+
+The script connects to a MySQL server running on localhost at port 3306 and
+fetches all rows in the states table with the name matching the argument,
+sorted in ascending order by id.
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    # Retrieve command-line arguments for MySQL credentials and state name
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Establish a connection to the MySQL database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=mysql_database
+    )
+
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
+
+    # Execute a query to select rows from states where name
+    # matches the state_name
+    query = """SELECT * FROM states WHERE BINARY name = '{}'
+ORDER BY id ASC""".format(state_name)
+    cursor.execute(query)
+
+    # Fetch all results from the executed query
+    states = cursor.fetchall()
+
+    # Print each state in the results
+    for state in states:
+        print(state)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
